33c032f1f4183c560dd3273ebf35af3a4769fc77
erikdvoss <erik@Eriks-MacBook-Air.local>
2015-07-14 17:35:05 -0400

    Finished get post pull delete

--- todomvc/todo/models.py
+++ todomvc/todo/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=140)
+    completed = models.BooleanField(default=False)
+    order = models.IntegerField(null=True)
