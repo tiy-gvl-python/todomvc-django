a99741fbe6d1c5b3d3452c9f28ae3897a11ab85b
PJ Passalacqua <pjpassa@gmail.com>
2015-07-15 09:03:52 -0400

    finsished easy mode

--- todomvc/todo/views.py
+++ todomvc/todo/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics, serializers
+from todo.models import Todo
+
+
+class TodoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Todo
+
+
+class ListCreateTodoView(generics.ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+class RetrieveUpdateDestroyTodoView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
